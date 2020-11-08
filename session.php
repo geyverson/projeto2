8f2e551f25c89446a05dfe4e08d9d9fe37dea08c
geyverson <54691524+geyverson@users.noreply.github.com>
2020-11-08 00:04:04 -0300

    Add files via upload

---
+++ session.php
@@ -0,0 +1,38 @@
+<?php
+
+return array(
+
+	
+
+	'driver' => 'native',
+
+	
+
+	'lifetime' => 120,
+
+	
+
+	'files' => storage_path().'/sessions',
+
+
+	'connection' => null,
+
+	
+
+	'table' => 'sessions',
+
+	
+	'lottery' => array(2, 100),
+
+	
+
+	'cookie' => 'laravel_session',
+
+	
+
+	'path' => '/',
+
+	
+	'domain' => null,
+
+);
---
+++ view.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+
+
+	'paths' => array(__DIR__.'/../views'),
+
+
+	'pagination' => 'pagination::slider',
+
+);
